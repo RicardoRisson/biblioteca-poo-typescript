543e3c067692be763f4aa184738aa6cabcb9eb5d
OctavioFPB <octaviopetry@gmail.com>
2025-05-20 20:38:48 -0300

    Autor.ts

--- br.upf.ccc.bibliteca.model/Autor.ts
+++ br.upf.ccc.bibliteca.model/Autor.ts
@@ -1,5 +1,5 @@
 class Autor extends Pessoa {
-    constructor(nome: string, email: string, public biografia?: string) {
+    constructor(nome: string, email: string, public biografia?: string) { //biografia com ? quer dizer que não é obrigatório na Instancia
         super(nome, email); // Chama o construtor da classe Pessoa
     }
 }
