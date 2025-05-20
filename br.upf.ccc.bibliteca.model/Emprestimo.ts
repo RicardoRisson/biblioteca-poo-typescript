c7c43f4357fb7e5b0c26873ca476fde25cb88773
OctavioFPB <octaviopetry@gmail.com>
2025-05-20 20:58:31 -0300

    Emprestimo.ts

--- br.upf.ccc.bibliteca.model/Emprestimo.ts
+++ br.upf.ccc.bibliteca.model/Emprestimo.ts
@@ -1,12 +1,15 @@
+import { Livro } from "./Livro"; // se estiver em arquivo separado
+
 export class Emprestimo {
   public dataEmprestimo: Date;
   public dataDevolucao?: Date;
   
   constructor(
-    public livro: string,
+    public livro: Livro,
     public dataDevolucao: Date
   ) {
     this.dataEmprestimo = new Date();
+    this.dataDevolucao = dataDevolucao;
   }
 
   realizarEmprestimo(): string {
