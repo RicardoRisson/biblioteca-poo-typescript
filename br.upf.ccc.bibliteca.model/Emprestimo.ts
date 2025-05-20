edeaf3736f2cb40d783ebeaa3dd9256e51aa94e8
OctavioFPB <octaviopetry@gmail.com>
2025-05-20 20:45:19 -0300

    Emprestimo.ts

--- br.upf.ccc.bibliteca.model/Emprestimo.ts
+++ br.upf.ccc.bibliteca.model/Emprestimo.ts
@@ -1,7 +1,30 @@
 export class Emprestimo {
+  public dataEmprestimo: Date;
+  public dataDevolucao?: Date;
+  
   constructor(
     public livro: string,
     public dataEmprestimo: Date,
     public dataDevolucao: Date
   ) {}
+  
+  constructor(
+      public livro: Livro,
+      public usuario: Usuario
+  ) {
+      this.dataEmprestimo = new Date();
+  }
+
+  realizarEmprestimo(): string {
+      if (this.livro.verificarDisponibilidade()) {
+          this.livro.marcarComoEmprestado();
+          return `Empréstimo realizado com sucesso para o livro "${this.livro.titulo}"`;
+      } else {
+          return `Erro: o livro "${this.livro.titulo}" já está emprestado.`;
+      }
+  }
+}
+
+class Emprestimo {
+    
 }
