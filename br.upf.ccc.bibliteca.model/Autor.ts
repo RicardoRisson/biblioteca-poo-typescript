f56ccd65ec37dffad6e7ce929ee299e061b6d90f
OctavioFPB <octaviopetry@gmail.com>
2025-05-20 21:52:49 -0300

    Update Autor.ts
    Adicionei o import Pessoa, e permiti que a classe autor fosse exportada

--- br.upf.ccc.bibliteca.model/Autor.ts
+++ br.upf.ccc.bibliteca.model/Autor.ts
@@ -1,4 +1,6 @@
-class Autor extends Pessoa {
+import { Pessoa } from "./Pessoa";
+
+export class Autor extends Pessoa {
     constructor(nome: string, email: string, public biografia?: string) { //biografia com ? quer dizer que não é obrigatório na Instancia
         super(nome, email); // Chama o construtor da classe Pessoa
     }
