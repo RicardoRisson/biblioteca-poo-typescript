1e2e95d7caa999091cf288784c2b3d32ce584cbc
OctavioFPB <octaviopetry@gmail.com>
2025-05-28 21:32:41 -0300

    Update Autor.ts

--- br.upf.ccc.bibliteca.model/Autor.ts
+++ br.upf.ccc.bibliteca.model/Autor.ts
@@ -3,5 +3,6 @@ import { Pessoa } from "./Pessoa";
 export class Autor extends Pessoa {
     constructor(nome: string, email: string, public biografia?: string) { //biografia com ? quer dizer que não é obrigatório na Instancia
         super(nome, email); // Chama o construtor da classe Pessoa
+        this.biografia = biografia;        
     }
 }
